e86a29895e561775241792a760f2e5ce818ff5e5
Tom Davy <tomd200@mac.com>
2017-11-02 12:01:32 +0000

    stream2 project

--- get_album_list.py
+++ get_album_list.py
@@ -2,10 +2,10 @@ import re
 import json
 
 
-
 def parse_for_albums():
     
-    album_set = set()
+    album_set = set();
+    album_list = [];
 
     with open("./text_files/bd_web_page_copy.txt", 'r') as inFile:
         
@@ -15,14 +15,13 @@ def parse_for_albums():
         
         for line in inFile:
             
+            line = line[:-1];       # remove end-of-line char.
+            
             album = "";
             
-            # remove num plays and dates from end of line
+            rev_line = line[::-1];  # reverse line
             
-            rev_line = line[::-1]; # reverse line
-            rev_line = rev_line[1:] # remove "/n"
-            
-            if rev_line[0] == "0" and re.search(number,rev_line[1]) is None:
+            if rev_line[0] == "0" and (re.search(number,rev_line[1]) is None or rev_line.find(" ") > 3):
                 rev_line = rev_line[1:];
             else:
                 rev_line = rev_line.split(" ", 1)[1];
@@ -40,6 +39,8 @@ def parse_for_albums():
                     album_pos = x + 1
                     album_name = line[album_pos:];
                     album_name = album_name.strip();
+                    if album_name == "Another Side Of Bob Dylan Jul 04, 1978 Jul 08, 2012 26":
+                        print(line);
                     album_set.add(album_name);
                 x += 1
             
@@ -47,29 +48,42 @@ def parse_for_albums():
     #  additional albums to add to list. 
     album_set.add('“Love And Theft”');
     
+    # convert to list in order to sort
+    album_list = list(album_set);
+    album_list.sort()
+    
     # output album list to file.
     outFile = open("./text_files/album_list.txt", "+w")
-    for album in album_set:
+    for x, album in enumerate(album_list):
         outFile.write(album);
-        outFile.write("\n");
+        if x < len(album_list) - 1:
+            outFile.write("\n");
     outFile.close();    
     
-    return album_set;
+    return;
 
     
 
-def check_for_other_albums(album_set):
+def check_for_other_albums():
     
+    album_list = [];
+
+    with open("./text_files/album_list.txt", "r") as albumFile:
+        for line in albumFile:
+            line = line[:-1]
+            album_list.append(line);
+    albumFile.close();
+        
     outFile = open("./text_files/missing_albums.txt", "+w");
     
     with open("./text_files/bd_web_page_copy.txt", 'r') as inFile:
         for line in inFile:
             pos = -1;
             found = 0;
-            for album in album_set:
-                pos = line.find(album)
+            for album in album_list:
+                pos = line.find(album);
                 if pos > -1:
-                    found = 1
+                    found = 1;
             if found == 0:
                 outFile.write(line);
         
@@ -78,8 +92,8 @@ def check_for_other_albums(album_set):
 
 album_set_1 = parse_for_albums()     
 
-# check_for_other_albums(album_set_1)  
-# print("check complete")
+check_for_other_albums()  
+print("check complete")
     
 
 
--- parse_song_list.py
+++ parse_song_list.py
@@ -1,8 +1,5 @@
 import re
-from flask import Flask, request, render_template
 from pymongo import MongoClient
-import os
-import tweepy
 import json
 
 from auth import MONGODB_URI
@@ -12,20 +9,23 @@ from auth import MONGODB_URI
 def parse_text():
     
     album_list = [];
-    number = "[0-9]"
-    
+    song_list = []
+
     with open("./text_files/album_list.txt", "r") as albumFile:
         for line in albumFile:
             line = line[:-1]
             album_list.append(line);
     albumFile.close();
-    
-    song_list = []
 
     with open("./text_files/bd_web_page_copy.txt", 'r') as inFile:
+        
         for line in inFile:
             
-            song = {}
+            line = line[:-1]; # remove end-of-line char.            
+            
+            # print(line);
+            
+            song = {};
             
             song_title = "";
             album = "";
@@ -34,85 +34,83 @@ def parse_text():
             last_date = "";
             num_plays = 0;
             
-            # capture album if it exists. 
-        # for line in inFile:
-        #     pos = -1;
-        #     found = 0;
-        #     for album in album_set:
-        #         pos = line.find(album)
-        #         if pos > -1:
-        #             found = 1
-        #     if found == 0:
-        #         outFile.write(line);
-
+            # Search for album and remove if exists
             pos = -1;
-            for a in album_list:
-                pos = line.find(a)
-                print(pos)
-                if pos > -1:
-                    print("found")
-                    album = a;
-            print(album)
-            
-            if album != "":
-                line = line.replace(album, '')            
-            # print(line);
-            
-            rev_line = line[::-1];      # reverse the line 
-            rev_line = rev_line[1:]     # remove end-of-line char 
+            len_album = 0;
 
-            if rev_line[0] == "0" and re.search(number,rev_line[1]) is None:
-                num_plays = 0;
-                dates = "";
-                rev_line = rev_line[1:];
-            else:
-                # print(rev_line)
-                # num_plays = int(rev_line.split(" ",1)[0]);  # Find first space after number of plays
-                rev_line = rev_line.split(" ", 1)[1];       
-                dates = rev_line[:25];
-                rev_line = rev_line[25:];
-                line = rev_line[::-1];
+            for a in album_list:
+                pos = line.find(a);
+                if pos > 0: # 0 and not -1 this is because a song may have the same name as an album
+                    if len(a) > len_album:  # Some album names are also within larger album names
+                        len_album = len(a);
+                        album = a;
+
+            if album != "":
+                line = line.replace(album, ' ')   ;
+            
+            # Line now does not include the album name.
+            # Find dates
+            
+            date_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            
+            pos = -1;
+            min_pos = -1;
+            for date in date_list:
+                date = " " + date + " "
+                pos = line.find(date);
+                if pos > -1:
+                    if min_pos == -1:
+                        min_pos = pos;
+                    elif min_pos > pos:
+                        min_pos = pos
+                        
+            if min_pos > -1:
+                dates = line[min_pos:min_pos + 26];
+                dates = dates.strip();
                 
-                dates = dates[::-1];
+                line = line.replace(dates, ' '); 
+                
                 dates = dates.split(" ");
                 dates = " ".join(dates[:3]), " ".join(dates[3:]);
                 first_date = dates[0];
                 last_date = dates[1];
 
-            # print("song: {0}".format(song_title, album, dates, num_plays))
-            # print("album: {1}".format(song_title, album, dates, num_plays))
-            # print("dates: {2}".format(song_title, album, dates, num_plays))
-            # print("num plays: {3}".format(song_title, album, dates, num_plays))
-        
+            # grabn chars befre the first space from end    
+            rev_line = line[::-1];      # reverse the line
+            end_chars = rev_line.split(" ",1);
 
-            # song['song_title'] = song_title;
-            # song['album'] = album;
-            # song["first_date"] = first_date;
-            # song["last_date"] = last_date;
-            # song["num_plays"] = num_plays
+            if end_chars[0].isdigit():
+                num_plays = int(end_chars[0][::-1]);
+                song_title = end_chars[1][::-1].strip();
+            else:
+                num_plays = 0;
+                song_title = rev_line[1:][::-1].strip();    # strip off 0 char and reverse
+
+            # print("song: {0}".format(song_title));
+            # print("album: {0}".format(album));
+            # print("first date: {0}".format(first_date));
+            # print("last date: {0}".format(last_date));
+            # print("num plays: {0}".format(num_plays));
+            # print("\n")
+
+            song['song_title'] = song_title;
+            song['album'] = album;
+            song["first_date"] = first_date;
+            song["last_date"] = last_date;
+            song["num_plays"] = num_plays
                 
             song_list.append(song);
 
-            
-            # outFile.write(song_title + ",");
-            # outFile.write(album + ",");
-            # outFile.write(first_date + ",");
-            # outFile.write(last_date + ",");
-            # outFile.write(str(num_plays)); 
-            # outFile.write("\n");
-    
-    # outFile = open("output.json", "+w")    
-    # outFile.write(json.dumps(song_list));
-    # outFile.close()
-    
-    # print (song_list)
+    outFile = open("output.json", "+w")    
+    outFile.write(json.dumps(song_list));
+    outFile.close()
     
     inFile.close()
     return song_list
 
 
 
-def load_mongo(song_list):
+def upload_mongo(song_list):
     
     database_name = "stream2_project";
     collection_name= "bob_dylan_songs";
@@ -130,10 +128,47 @@ def load_mongo(song_list):
         outFile.close()
          
         collection.insert_many(song_list)
+
         
+def download_mongo():
+    
+    database_name = "stream2_project";
+    collection_name= "bob_dylan_songs";
+    
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[database_name];
+        collection = db[collection_name];
+
+        song_list = list(collection.find({}));
         
-song_list = parse_text()     
-# print(song_list)
-# load_mongo(song_list)
+    return song_list
+
+def verify_data(song_list):
+    
+    album_set = set();
+    song_set = set();
+    id_set = set();
+    
+    for song in song_list:
+        album_set.add(song["album"]);
+        song_set.add(song["song_title"]);
+        id_set.add(song["_id"]);
+    
+    print("Total records: {0}".format(len(song_list)));    
+    print("Total unique albums: {0}".format(len(album_set)));    
+    print("Total unique songs: {0}".format(len(song_set)));    
+    print("Total unique ids (mongo): {0}".format(len(id_set)));    
+    
+    return
+        
+song_list = parse_text();     
+upload_mongo(song_list);
+
+song_list = download_mongo();
+verify_data(song_list);
+
+
+
+
                         
                     
