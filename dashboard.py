70f7767e86e297a098145ca5da340856945a838f
Tom Davy <tomd200@mac.com>
2017-11-06 16:24:08 +0000

    stream2 project

--- dashboard.py
+++ dashboard.py
@@ -4,15 +4,9 @@ from pymongo import MongoClient
 import json
 import os
 
-from auth import MONGODB_URI
+# from auth import MONGODB_URI
 
 app = Flask(__name__)
-
-# mongo_host = 'localhost'
-# mongo_port = 43805
-
-# dbs_name = 'stream2_project'
-# collection_name = 'bob_dylan_songs'
 
 mongodb_uri = os.environ.get('mongodb_uri')
 dbs_name = os.environ.get('mongo_db_name','stream2_project')
@@ -25,15 +19,12 @@ def index():
     return render_template("index.html")
 
 @app.route("/songs")
-def bob_dylan_songs():
-    with MongoClient(MONGODB_URI) as conn:
-        # define which collection we wish to access
+def get_songs():
+    with MongoClient(mongodb_uri) as conn:
         collection = conn[dbs_name][collection_name]
-        # retrieve a result set only with the fields defined in fields
-        # and limit the the results to 55000
         songs = collection.find(projection=fields)
-        # convert projects to a list in a json object and return the json data
         return json.dumps(list(songs))
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('ip', '0.0.0.0'),port=int(os.getenv('port', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('port', 8080)))
+
