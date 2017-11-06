e2c50b26bd8e3d2646bfdf4e5344d0e9ed0b0a91
Tom Davy <tomd200@mac.com>
2017-11-06 22:16:31 +0000

    stream2 project

--- download_json.py
+++ download_json.py
@@ -6,18 +6,21 @@ import os
 
 from auth import MONGODB_URI
 
-mongo_db_name = "stream2_project"
-collection_name = "bob_dylan_songs"
-
-fields = {'song_title': True, 'song_chart_pos': True, 'album': True, 'album_year': True, 'first_date': True, 'last_date': True, 'num_plays': True, '_id': False}
-
-with MongoClient(MONGODB_URI) as conn:
-    db = conn[mongo_db_name];
-    collection = db[collection_name];
-    song_list = collection.find(projection=fields);
-
-song_list = list(song_list);
+def download_json(collection_name, file_name):
+    mongo_db_name = "stream2_project"
     
-outFile = open("bob_dylan_songs.json", "+w");
-outFile.write(json.dumps(song_list));
-outFile.close();
+    fields = {'song_title': True, 'song_chart_pos': True, 'album': True, 'album_year': True, 'first_date': True, 'last_date': True, 'num_plays': True, '_id': False}
+    
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[mongo_db_name];
+        collection = db[collection_name];
+        song_list = collection.find(projection=fields);
+    
+    song_list = list(song_list);
+        
+    outFile = open(file_name, "+w");
+    outFile.write(json.dumps(song_list));
+    outFile.close();
+    return;
+    
+download_json("top_30_songs", "top_30_songs.json");
